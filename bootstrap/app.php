24e19ce488224ae0d31b0c528d06be91bdcf1a4d
Kevin ANATOLE <kevanatole@gmail.com>
2020-10-29 07:50:08 +0000

    Firebase Auth configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,6 +79,10 @@ $app->configure('app');
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
 
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\FirebaseMiddleware::class,
+]);
+
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
